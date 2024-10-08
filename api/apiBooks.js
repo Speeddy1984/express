c6f054360746f8897b8ff6ced3c3856a7b7973a7
Speeddy1984 <speeddy1984@yandex.ru>
2024-10-08 09:12:32 +0800

    mongoose hw

--- api/apiBooks.js
+++ api/apiBooks.js
@@ -1,116 +1,79 @@
 const express = require("express");
-const { v4: uuidv4 } = require("uuid");
+// const { v4: uuidv4 } = require("uuid");
 const router = express.Router();
-const upload = require("../middleware/uploadMiddleware");
+// const upload = require("../middleware/uploadMiddleware");
+const Book = require("../models/book");
 
-class Book {
-  constructor(
-    title,
-    description,
-    authors,
-    favorite,
-    fileCover,
-    fileName,
-    fileBook
-  ) {
-    this.id = uuidv4();
-    this.title = title;
-    this.description = description;
-    this.authors = authors;
-    this.favorite = favorite;
-    this.fileCover = fileCover;
-    this.fileName = fileName;
-    this.fileBook = fileBook;
+// Получить список всех книг
+
+router.get("/", async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-}
-
-let books = [];
-
-// Получить все книги
-router.get("/", (req, res) => {
-  res.json(books);
 });
 
 // Получить книгу по ID
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  const book = books.find((b) => b.id === id);
 
-  if (book) {
-    res.json(book);
-  } else {
-    res.status(404).send("Книга не найдена");
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const book = await Book.findById(id);
+    if (book) {
+      res.json(book);
+    } else {
+      res.status(404).json({ message: "Книга не найдена" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 
-// Создать книгу с загрузкой файла
-router.post("/", upload.single("fileBook"), (req, res) => {
-  const { title, description, authors, favorite, fileCover, fileName } =
-    req.body;
-  const fileBook = req.file ? req.file.path : "";
-  const newBook = new Book(
-    title,
-    description,
-    authors,
-    favorite,
-    fileCover,
-    fileName,
-    fileBook
-  );
-  books.push(newBook);
-  res.status(201).json(newBook);
+// Создать книгу
+
+router.post("/", async (req, res) => {
+  try {
+    const newBook = new Book(req.body);
+    await newBook.save();
+    res.status(201).json(newBook);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // Редактировать книгу по ID
-router.put("/:id", (req, res) => {
-  const { id } = req.params;
-  const {
-    title,
-    description,
-    authors,
-    favorite,
-    fileCover,
-    fileName,
-    fileBook,
-  } = req.body;
-  const book = books.find((b) => b.id === id);
 
-  if (book) {
-    book.title = title || book.title;
-    book.description = description || book.description;
-    book.authors = authors || book.authors;
-    book.favorite = favorite !== undefined ? favorite : book.favorite;
-    book.fileCover = fileCover || book.fileCover;
-    book.fileName = fileName || book.fileName;
-    book.fileBook = fileBook || book.fileBook;
-    res.json(book);
-  } else {
-    res.status(404).send("Книга не найдена");
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (updatedBook) {
+      res.json(updatedBook);
+    } else {
+      res.status(404).json({ message: "Книга не найдена" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 
 // Удалить книгу по ID
-router.delete("/:id", (req, res) => {
+
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  const bookIndex = books.findIndex((b) => b.id === id);
-
-  if (bookIndex !== -1) {
-    books.splice(bookIndex, 1);
-    res.send("ok");
-  } else {
-    res.status(404).send("Книга не найдена");
-  }
-});
-
-// Скачать файл книги по ID
-router.get("/:id/download", (req, res) => {
-  const { id } = req.params;
-  const book = books.find((b) => b.id === id);
-
-  if (book && book.fileBook) {
-    res.download(book.fileBook);
-  } else {
-    res.status(404).send("Файл не найден");
+  try {
+    const deletedBook = await Book.findByIdAndDelete(id);
+    if (deletedBook) {
+      res.json({ message: "Книга удалена" });
+    } else {
+      res.status(404).json({ message: "Книга не найдена" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 
--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const express = require('express');
 const err404 = require('./middleware/err404')
 const err5XX = require('./middleware/err5XX')
 const app = express();
+
+const mongoose = require('mongoose');
+
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/library';
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Успешно подключено к MongoDB'))
+.catch((err) => console.error('Ошибка подключения к MongoDB:', err));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
 const booksRouter = require('./routes/books');
-const apiBooksRouter = require('./api/apiBooks');  // Роуты для API, оставить для будущих задач
+const apiBooksRouter = require('./api/apiBooks');  // Роуты для API
 
 // Авторизация пользователя, пока оставляем. Возможно, пригодится позже
 app.post('/api/user/login', (req, res) => {
